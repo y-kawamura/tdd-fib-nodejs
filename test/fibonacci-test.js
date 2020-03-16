21197baa19fd71c5220a94d667e7ce052acbe95d
y-kawamura <tetora.y@gmail.com>
2020-03-16 16:20:53 +0900

    Create fib(1) = 1 test

--- test/fibonacci-test.js
+++ test/fibonacci-test.js
@@ -5,5 +5,8 @@ const fib = require('../src/fibnacci');
 describe('Fibonacci function test', () => [
   it('fib(0) = 0', () => {
     assert.equal(fib(0), 0);
-  })
+  }),
+  it('fib(1) = 1', () => {
+    assert.equal(fib(1), 1);
+  }),
 ]);
